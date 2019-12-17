a65afe34deb244fbb575d1b93691f2b474fca927
Harry Pierson <harrypierson@outlook.com>
2019-12-17 12:07:37 -0800

    more cleanup

--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -6,17 +6,37 @@ using System.Threading.Tasks;
 
 namespace TestClient
 {
-    class Program
+    internal interface IServer
     {
-        static async Task Main(string[] args)
+        event EventHandler<string> MessageArrived;
+        Task<int> Add(int a, int b);
+        Task SendMessage(string message);
+    }
+
+    internal static class Program
+    {
+        private static async Task Main()
         {
-            var client = new TcpClient();
-            await client.ConnectAsync("localhost", 5150);
-            var jsonRpc = JsonRpc.Attach(client.GetStream());
+            using var client = new TcpClient();
+            await client.ConnectAsync("localhost", 5150).ConfigureAwait(false);
+            var jsonRpc = JsonRpc.Attach<IServer>(client.GetStream());
+            jsonRpc.MessageArrived += JsonRpc_MessageArrived;
+
             var now = DateTimeOffset.Now;
             Console.WriteLine($"{now.Minute} + {now.Second}");
-            var sum = await jsonRpc.InvokeAsync<int>("Add", now.Minute, now.Second);
+            var sum = await jsonRpc.Add(now.Minute, now.Second).ConfigureAwait(false);
             Console.WriteLine(sum);
+
+            await jsonRpc.SendMessage("Hello, World!").ConfigureAwait(false);
+
+            Console.WriteLine("About to wait");
+            await Task.Delay(4000).ConfigureAwait(false);
+            Console.WriteLine("wait Completed");
+        }
+
+        private static void JsonRpc_MessageArrived(object? sender, string e)
+        {
+            Console.WriteLine($"MessageArrived: {e}");
         }
     }
 }
--- TestServer/Server.cs
+++ TestServer/Server.cs
@@ -1,8 +1,10 @@
+using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
 namespace TestServer
 {
-    class Server
+    internal class Server
     {
         private readonly ILogger<Server> logger;
 
@@ -13,8 +15,21 @@ namespace TestServer
 
         public int Add(int a, int b)
         {
-            logger.LogInformation("Received request: {0} + {1}", a, b);
+            logger.LogInformation("Received Add: {0} + {1}", a, b);
             return a + b;
+        }
+
+        public event EventHandler<string>? MessageArrived;
+
+        public void SendMessage(string message)
+        {
+            logger.LogInformation("Received SendMessage: {0}", message);
+
+            Task.Run(async () =>
+            {
+                await Task.Delay(2000).ConfigureAwait(false);
+                MessageArrived?.Invoke(this, message);
+            });
         }
     }
 }
